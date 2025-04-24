d493f6f05a372840faaf651fd4aac5c7b1b0198f
nagul2 <hayani0000@gmail.com>
2025-04-24 23:52:33 +0900

    Refactor[Users]: PR 수정사항 반영

--- src/main/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImpl.java
+++ src/main/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImpl.java
@@ -32,6 +32,8 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public UserResponseDto updateUserById(Long id, AuthUser authUser, UpdateUserRequestDto requestDto) {
         Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
+        users.validateAccess(authUser); // 로그인한 본인인지 검증
+        users.validateDelete();         // 삭제 검증
 
         // 닉네임 변경 시 변경할 닉네임이 DB에 있으면 예외 발생
         if (!users.getNickname().equals(requestDto.getNickname()) &&
@@ -39,8 +41,7 @@ public class UserServiceImpl implements UserService {
             throw new GlobalException(ErrorCode.USER_EXIST_NICKNAME);
         }
 
-        users.validateAccess(authUser); // 로그인한 본인인지 검증
-        users.validateDelete();         // 삭제 검증
+
         users.updateBy(requestDto);     // 업데이트
 
         return UserResponseDto.from(users);
