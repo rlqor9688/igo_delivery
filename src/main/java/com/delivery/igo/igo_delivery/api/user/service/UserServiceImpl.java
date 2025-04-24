dc0f7fc62170df5fe0ae333b2f2c3073710d9c89
nagul2 <hayani0000@gmail.com>
2025-04-24 21:40:51 +0900

    Feature[Users]: 내 정보 수정 API 개발
    1. 내 정보 수정 API 개발 관련 코드들 추가
    2. NotNull 검증 기본 메시지 추가

--- src/main/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImpl.java
+++ src/main/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.delivery.igo.igo_delivery.api.user.service;
 
+import com.delivery.igo.igo_delivery.api.user.dto.request.UpdateUserRequestDto;
 import com.delivery.igo.igo_delivery.api.user.dto.resonse.UserResponseDto;
 import com.delivery.igo.igo_delivery.api.user.entity.Users;
 import com.delivery.igo.igo_delivery.api.user.repository.UserRepository;
 import com.delivery.igo.igo_delivery.common.dto.AuthUser;
+import com.delivery.igo.igo_delivery.common.exception.AuthException;
 import com.delivery.igo.igo_delivery.common.exception.ErrorCode;
 import com.delivery.igo.igo_delivery.common.exception.GlobalException;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,23 @@ public class UserServiceImpl implements UserService {
         
         users.validateAccess(authUser); // 로그인한 본인인지 검증
         users.validateDelete(); // 삭제 검증, 삭제된 상태라면 404 예외 발생
+
+        return UserResponseDto.from(users);
+    }
+
+    @Override
+    public UserResponseDto updateUserById(Long id, AuthUser authUser, UpdateUserRequestDto requestDto) {
+        Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
+
+        // 닉네임 변경 시 변경할 닉네임이 DB에 있으면 예외 발생
+        if (!users.getNickname().equals(requestDto.getNickname()) &&
+                userRepository.existsByNickname(requestDto.getNickname())) {
+            throw new AuthException(ErrorCode.USER_EXIST_NICKNAME);
+        }
+
+        users.validateAccess(authUser); // 로그인한 보인인지 검증
+        users.validateDelete();         // 삭제 검증
+        users.updateBy(requestDto);     // 업데이트
 
         return UserResponseDto.from(users);
     }
