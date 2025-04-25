b54215d0d21f1be35b11947c9142b4fea56b357a
nagul2 <hayani0000@gmail.com>
2025-04-25 15:03:38 +0900

    Refactor[Users]: 회원을 조회하고 검증하는 로직이 모든 메서드에 공통으로 되어있어 이를 private메서드로 수정
    1. validateAccess, validateDelete, user 조회 후 없으면 예외 던지는 코드를

--- src/main/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImpl.java
+++ src/main/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImpl.java
@@ -25,9 +25,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserResponseDto findUserById(Long id, AuthUser authUser) {
-        Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-        users.validateAccess(authUser); // 로그인한 본인인지 검증
-        users.validateDelete(); // 삭제 검증, 삭제된 상태라면 404 예외 발생
+        Users users = getValidatedUser(id, authUser);   // 검증된 유저를 꺼내는 메서드
 
         return UserResponseDto.from(users);
     }
@@ -35,9 +33,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public UserResponseDto updateUserById(Long id, AuthUser authUser, UpdateUserRequestDto requestDto) {
-        Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-        users.validateAccess(authUser); // 로그인한 본인인지 검증
-        users.validateDelete();         // 삭제 검증
+        Users users = getValidatedUser(id, authUser);   // 검증된 유저를 꺼내는 메서드
 
         // 닉네임 변경 시 변경할 닉네임이 DB에 있으면 예외 발생
         if (!users.getNickname().equals(requestDto.getNickname()) &&
@@ -58,9 +54,7 @@ public class UserServiceImpl implements UserService {
             throw new GlobalException(ErrorCode.PASSWORD_DUPLICATED);
         }
 
-        Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-        users.validateAccess(authUser); // 로그인한 본인인지 검증
-        users.validateDelete();         // 삭제 검증
+        Users users = getValidatedUser(id, authUser);   // 검증된 유저를 꺼내는 메서드
 
         if (!passwordEncoder.matches(requestDto.getOldPassword(), users.getPassword())) {
             throw new GlobalException(ErrorCode.PASSWORD_NOT_MATCHED);
@@ -73,14 +67,21 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public void deleteUser(Long id, AuthUser authUser, DeleteUserRequestDto requestDto) {
-        Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
-        users.validateAccess(authUser); // 로그인한 본인인지 검증
-        users.validateDelete();
+        Users users = getValidatedUser(id, authUser);   // 검증된 유저를 꺼내는 메서드
 
         if (!passwordEncoder.matches(requestDto.getPassword(), users.getPassword())) {
             throw new GlobalException(ErrorCode.PASSWORD_NOT_MATCHED);
         }
 
         users.delete();
+    }
+
+    // UserServiceImpl 모든 메서드에서 공통적으로 사용되는 User 검증을 private 메서드화 하여 재사용하도록 변경
+    private Users getValidatedUser(Long id, AuthUser authUser) {
+        // 유저가 없으면 예외
+        Users users = userRepository.findById(id).orElseThrow(() -> new GlobalException(ErrorCode.USER_NOT_FOUND));
+        users.validateAccess(authUser); // 로그인한 본인인지 검증
+        users.validateDelete();         // 삭제 되었는지 검증
+        return users;
     }
 }
