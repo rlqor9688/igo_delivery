1447a167c59e605680d370367527ab88fc065068
nagul2 <hayani0000@gmail.com>
2025-04-25 12:32:23 +0900

    Test[Users]: 비밀번호 수정 단위 테스트
    1. 재사용된 검증메서드 테스트는 패스
    2. 기존 비밀번호와 새로운 비밀번호를 같게 입력하면 예외 발생 테스트
    3. 기존 비밀번호와 DB에서 꺼낸 비밀번호를 매칭할 때 다르면 예외 발생 테스트

--- src/test/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImplUnitTest.java
+++ src/test/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImplUnitTest.java
@@ -1,11 +1,13 @@
 package com.delivery.igo.igo_delivery.api.user.service;
 
+import com.delivery.igo.igo_delivery.api.user.dto.request.UpdatePasswordRequestDto;
 import com.delivery.igo.igo_delivery.api.user.dto.request.UpdateUserRequestDto;
 import com.delivery.igo.igo_delivery.api.user.dto.resonse.UserResponseDto;
 import com.delivery.igo.igo_delivery.api.user.entity.UserRole;
 import com.delivery.igo.igo_delivery.api.user.entity.UserStatus;
 import com.delivery.igo.igo_delivery.api.user.entity.Users;
 import com.delivery.igo.igo_delivery.api.user.repository.UserRepository;
+import com.delivery.igo.igo_delivery.common.config.PasswordEncoder;
 import com.delivery.igo.igo_delivery.common.dto.AuthUser;
 import com.delivery.igo.igo_delivery.common.exception.ErrorCode;
 import com.delivery.igo.igo_delivery.common.exception.GlobalException;
@@ -26,6 +28,9 @@ class UserServiceImplUnitTest {
 
     @Mock
     UserRepository userRepository;
+
+    @Mock
+    PasswordEncoder passwordEncoder;
 
     @InjectMocks
     UserServiceImpl userService;
@@ -59,7 +64,7 @@ class UserServiceImplUnitTest {
     @Test
     void 내정보_조회시_내정보가_없으면_예외_발생_에러코드_USER_NOT_FOUND() {
         // given
-        AuthUser authUser = new AuthUser(1L, "email@naver.com", "로그인유저", UserRole.OWNER);
+        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
 
         // when & then
         GlobalException exception = assertThrows(GlobalException.class, () -> userService.findUserById(1L, authUser));
@@ -98,5 +103,44 @@ class UserServiceImplUnitTest {
         verify(userRepository).existsByNickname(requestDto.getNickname());
     }
 
+    @Test
+    void 기존_비밀번호와_변경할_비밀번호가_동일하게_요청이오면_예외_발생_ErrorCode_PASSWORD_DUPLICATED() {
+        // given
+        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
+        UpdatePasswordRequestDto requestDto = new UpdatePasswordRequestDto("samePassword", "samePassword");
+
+        // when & then
+        GlobalException exception = assertThrows(GlobalException.class,
+                () -> userService.updateUserPasswordById(1L, authUser, requestDto));
+        assertEquals(ErrorCode.PASSWORD_DUPLICATED, exception.getErrorCode());
+
+    }
+
+    @Test
+    void 입력한_비밀번호가_DB에서_조회된_비밀번호를_매칭할때_다르면_예외_발생_ErrorCode_PASSWORD_NOT_MATCHED() {
+        // given
+        Users user = Users.builder()
+                .id(1L)
+                .email("email@naver.com")
+                .nickname("정상유저")
+                .phoneNumber("010-1111-2222")
+                .address("세상에없는구")
+                .password("encodedPassword")
+                .userRole(UserRole.OWNER)
+                .userStatus(UserStatus.LIVE)
+                .build();
+
+        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
+        UpdatePasswordRequestDto requestDto = new UpdatePasswordRequestDto("oldPassword", "newPassword");
+
+        given(userRepository.findById(authUser.getId())).willReturn(Optional.of(user));
+        given(passwordEncoder.matches("oldPassword", "encodedPassword")).willReturn(false);
+
+        // when & then
+        GlobalException exception = assertThrows(GlobalException.class,
+                () -> userService.updateUserPasswordById(1L, authUser, requestDto));
+        assertEquals(ErrorCode.PASSWORD_NOT_MATCHED, exception.getErrorCode());
+
+    }
 }
 
