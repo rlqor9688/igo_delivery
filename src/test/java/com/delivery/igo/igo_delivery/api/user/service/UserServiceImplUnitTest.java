f240e24804c0a5203bddb9cdb67e5cfd53d5d76c
nagul2 <hayani0000@gmail.com>
2025-04-25 12:42:03 +0900

    Refactor[Users]: UserServiceImpl 단위 테스트 리렉토링
    1. 매번 생성하는 유저, AuthUser 코드를 BeforeEach로 수정

--- src/test/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImplUnitTest.java
+++ src/test/java/com/delivery/igo/igo_delivery/api/user/service/UserServiceImplUnitTest.java
@@ -11,6 +11,7 @@ import com.delivery.igo.igo_delivery.common.config.PasswordEncoder;
 import com.delivery.igo.igo_delivery.common.dto.AuthUser;
 import com.delivery.igo.igo_delivery.common.exception.ErrorCode;
 import com.delivery.igo.igo_delivery.common.exception.GlobalException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -35,20 +36,28 @@ class UserServiceImplUnitTest {
     @InjectMocks
     UserServiceImpl userService;
 
-    @Test
-    void 내정보가_정상적으로_조회됨() {
-        // given
-        Users user = Users.builder()
+    private Users user;
+    private AuthUser authUser;
+
+    @BeforeEach
+    void testInit() {
+        user = Users.builder()
                 .id(1L)
                 .email("email@naver.com")
                 .nickname("정상유저")
                 .phoneNumber("010-1111-2222")
                 .address("세상에없는구")
+                .password("encodedPassword")
                 .userRole(UserRole.OWNER)
                 .userStatus(UserStatus.LIVE)
                 .build();
 
-        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
+        authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
+    }
+
+    @Test
+    void 내정보가_정상적으로_조회됨() {
+        // given
         given(userRepository.findById(authUser.getId())).willReturn(Optional.of(user));
 
         // when
@@ -76,17 +85,6 @@ class UserServiceImplUnitTest {
     @Test
     void 내정보_수정시_변경할_닉네임이_이미있다면_에러_발생_에러코드_USER_EXIST_NICKNAME() {
         // given
-        Users user = Users.builder()
-                .id(1L)
-                .email("email@naver.com")
-                .nickname("정상유저")
-                .phoneNumber("010-1111-2222")
-                .address("세상에없는구")
-                .userRole(UserRole.OWNER)
-                .userStatus(UserStatus.LIVE)
-                .build();
-
-        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
         UpdateUserRequestDto requestDto = new UpdateUserRequestDto("수정할게요",
                 "010-1111-1111",
                 "주소",
@@ -101,12 +99,12 @@ class UserServiceImplUnitTest {
         assertEquals(ErrorCode.USER_EXIST_NICKNAME, exception.getErrorCode());
         verify(userRepository).findById(authUser.getId());
         verify(userRepository).existsByNickname(requestDto.getNickname());
+
     }
 
     @Test
     void 기존_비밀번호와_변경할_비밀번호가_동일하게_요청이오면_예외_발생_ErrorCode_PASSWORD_DUPLICATED() {
         // given
-        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
         UpdatePasswordRequestDto requestDto = new UpdatePasswordRequestDto("samePassword", "samePassword");
 
         // when & then
@@ -119,18 +117,6 @@ class UserServiceImplUnitTest {
     @Test
     void 입력한_비밀번호가_DB에서_조회된_비밀번호를_매칭할때_다르면_예외_발생_ErrorCode_PASSWORD_NOT_MATCHED() {
         // given
-        Users user = Users.builder()
-                .id(1L)
-                .email("email@naver.com")
-                .nickname("정상유저")
-                .phoneNumber("010-1111-2222")
-                .address("세상에없는구")
-                .password("encodedPassword")
-                .userRole(UserRole.OWNER)
-                .userStatus(UserStatus.LIVE)
-                .build();
-
-        AuthUser authUser = new AuthUser(1L, "email@naver.com", "정상유저", UserRole.OWNER);
         UpdatePasswordRequestDto requestDto = new UpdatePasswordRequestDto("oldPassword", "newPassword");
 
         given(userRepository.findById(authUser.getId())).willReturn(Optional.of(user));
