6ff7e6c9f43aba4b362a661035b5b26b09155288
se0yu <pr924133@gmail.com>
2025-04-27 11:45:36 +0900

    Fix[Cart] 장바구니 생성 request 오류 수정 1. request 변수가 string이 아니므로 notBlank 사용 불가 2. notNull로 변경

--- src/main/java/com/delivery/igo/igo_delivery/api/cart/dto/CartRequest.java
+++ src/main/java/com/delivery/igo/igo_delivery/api/cart/dto/CartRequest.java
@@ -1,7 +1,7 @@
 package com.delivery.igo.igo_delivery.api.cart.dto;
 
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -9,10 +9,10 @@ import lombok.Getter;
 @AllArgsConstructor
 public class CartRequest {
 
-    @NotBlank(message = "{menu.id.notblank}")
+    @NotNull(message = "{menu.id.notblank}")
     private Long menuId;
 
-    @NotBlank(message ="{cart.quantity.notnull}")
+    @NotNull(message ="{cart.quantity.notnull}")
     @Min(value = 1, message = "{cart.quantity.min}")
     private Integer cartQuantity;
 
