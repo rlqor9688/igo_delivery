e4e15ec6dc0b5d01e40e6f72417f62ab974666c6
nagul2 <hayani0000@gmail.com>
2025-04-28 11:19:26 +0900

    BugFix[Cart]: 장바구니 수량 증가/감소 API에 잘못된 Bean Validation 애노테이션 적용된것을 수정

--- src/main/java/com/delivery/igo/igo_delivery/api/cart/dto/request/UpdateCartItemRequestDto.java
+++ src/main/java/com/delivery/igo/igo_delivery/api/cart/dto/request/UpdateCartItemRequestDto.java
@@ -1,7 +1,7 @@
 package com.delivery.igo.igo_delivery.api.cart.dto.request;
 
 import com.delivery.igo.igo_delivery.api.cart.entity.CartItemQuantityType;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,6 +11,6 @@ public class UpdateCartItemRequestDto {
 
     // 증가 - INCREASE
     // 감소 - DECREASE
-    @NotBlank
+    @NotNull
     private final CartItemQuantityType actionType;
 }
